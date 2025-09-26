659a5929f62f8d2914e011caa6d871ef93c6db32
nuredinbedruyimer <nuredinbedru06@gmail.com>
2025-09-25 17:35:04 -0700

    Fix : Fix Error on Database connection by configure when we connect the database

--- database/database.js
+++ database/database.js
@@ -1,6 +1,11 @@
 import { Pool } from "pg";
+import dotenv from "dotenv";
+dotenv.config()
 
 // console.log(process.env.HOST)
+// console.log(process.env.PASSWORD)
+// console.log(process.env.USER)
+
 
 const pool = new Pool({
     host:process.env.HOST,
